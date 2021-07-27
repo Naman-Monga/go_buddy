77d3f6e7e27028ffa15715447193a325246cdf49
Naman Monga <namanmonga27@gmail.com>
2021-07-27 23:02:09 +0530

    makeover1

--- streaming/models.py
+++ streaming/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Room(models.Model):
     admin = models.IntegerField()
     room_name = models.CharField(max_length=1000)
-
+    room_title = models.CharField(max_length=1000, null=True, blank=True)
     def __str__(self) -> str:
         return self.room_name
--- streaming/views.py
+++ streaming/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Room
+import uuid 
 # Create your views here.
 
 
@@ -8,16 +9,15 @@ def room(request, room_name):
     iamadmin = 0
     if(curr_room.admin==request.user.id):
         iamadmin=1
-    context = {'room_name':room_name, 'admin':curr_room.admin,'iamadmin':iamadmin}
+    context = {'room_name':room_name, 'admin':curr_room.admin,'iamadmin':iamadmin, 'room_title':curr_room.room_title}
     return render(request, 'streaming/index.html', context)
 
 def create_room(request):
 
     if request.method=="POST":
-        room_name = request.POST.get('roomName')
-        if room_name=="":
-            return render(request, 'streaming/createRoom.html')
-        myroom = Room( admin=request.user.id, room_name=room_name)
+        room_title = request.POST.get('roomName')
+        room_name = uuid.uuid4().hex[:10] # Creates a random id of 10 characters.
+        myroom = Room( admin=request.user.id, room_name=room_name, room_title=room_title)
         myroom.save()
         return redirect('room', room_name=room_name)
 
